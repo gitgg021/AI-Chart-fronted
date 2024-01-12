5a8af73f13d93b72a9547ac0ab3e196b0ab3fc6e
caoxuehan <caoxuehan@qq.com>
2024-01-12 14:20:16 +0800

    前端初始化

---
+++ src/pages/User/Login/index.tsx
@@ -0,0 +1,170 @@
+import Footer from '@/components/Footer';
+import {
+  AlipayCircleOutlined,
+  LockOutlined,
+  TaobaoCircleOutlined,
+  UserOutlined,
+  WeiboCircleOutlined,
+} from '@ant-design/icons';
+import { LoginForm, ProFormText } from '@ant-design/pro-components';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { Helmet, history, useModel } from '@umijs/max';
+import { Alert, message, Tabs } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { flushSync } from 'react-dom';
+import Settings from '../../../../config/defaultSettings';
+import { listChartByPageUsingPost } from '@/services/caocaobi/chartController';
+import { Link } from '@@/exports';
+import { getLoginUserUsingGet, userLoginUsingPost } from '@/services/caocaobi/userController';
+
+
+
+const Login: React.FC = () => {
+  const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
+  const [type, setType] = useState<string>('account');
+  const { initialState, setInitialState } = useModel('@@initialState');
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    };
+  });
+
+  useEffect(() => {
+    listChartByPageUsingPost({}).then(res =>{
+      console.error('res',res)
+    })
+  })
+
+  /**
+   * 登录成功后获取用户的登录信息
+   */
+  const fetchUserInfo = async () => {
+    const userInfo = await getLoginUserUsingGet();
+    if (userInfo) {
+      flushSync(() => {
+        setInitialState((s) => ({
+          ...s,
+          currentUser: userInfo,
+        }));
+      });
+    }
+  };
+  const handleSubmit = async (values: API.UserLoginRequest) => {
+    try {
+      // 登录
+      const res = await userLoginUsingPost(values);
+      if (res.code === 0) {
+        const defaultLoginSuccessMessage = '登录成功！';
+        message.success(defaultLoginSuccessMessage);
+        await fetchUserInfo();
+        const urlParams = new URL(window.location.href).searchParams;
+        history.push(urlParams.get('redirect') || '/');
+        return;
+      }else {
+        message.error(res.message)
+      }
+
+    } catch (error) {
+      const defaultLoginFailureMessage = '登录失败，请重试！';
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  const { status, type: loginType } = userLoginState;
+  return (
+    <div className={containerClassName}>
+      <Helmet>
+        <title>
+          {'登录'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" src="/logo.svg" />}
+          title="CC智能 BI"
+          subTitle={'CC智能 BI 旨于提供更便捷的BI服务'}
+
+          onFinish={async (values) => {
+            await handleSubmit(values as API.UserLoginRequest);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '账户密码登录',
+              },
+            ]}
+          />
+
+
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={'请输入用户名'}
+                rules={[
+                  {
+                    required: true,
+                    message: '用户名是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                ]}
+              />
+            </>
+          )}
+
+
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+           <Link
+              to="/user/register"
+            >
+             注册
+           </Link>
+           </div>
+        </LoginForm>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Login;
---
+++ src/services/caocaobi/typings.d.ts
@@ -0,0 +1,475 @@
+declare namespace API {
+  type BaseResponseBoolean_ = {
+    code?: number;
+    data?: boolean;
+    message?: string;
+  };
+
+  type BaseResponseChart_ = {
+    code?: number;
+    data?: Chart;
+    message?: string;
+  };
+
+  type BaseResponseInt_ = {
+    code?: number;
+    data?: number;
+    message?: string;
+  };
+
+  type BaseResponseLoginUserVO_ = {
+    code?: number;
+    data?: LoginUserVO;
+    message?: string;
+  };
+
+  type BaseResponseLong_ = {
+    code?: number;
+    data?: string;
+    message?: string;
+  };
+
+  type BaseResponsePageChart_ = {
+    code?: number;
+    data?: PageChart_;
+    message?: string;
+  };
+
+  type BaseResponsePagePost_ = {
+    code?: number;
+    data?: PagePost_;
+    message?: string;
+  };
+
+  type BaseResponsePagePostVO_ = {
+    code?: number;
+    data?: PagePostVO_;
+    message?: string;
+  };
+
+  type BaseResponsePageUser_ = {
+    code?: number;
+    data?: PageUser_;
+    message?: string;
+  };
+
+  type BaseResponsePageUserVO_ = {
+    code?: number;
+    data?: PageUserVO_;
+    message?: string;
+  };
+
+  type BaseResponsePostVO_ = {
+    code?: number;
+    data?: PostVO;
+    message?: string;
+  };
+
+  type BaseResponseString_ = {
+    code?: number;
+    data?: string;
+    message?: string;
+  };
+
+  type BaseResponseUser_ = {
+    code?: number;
+    data?: User;
+    message?: string;
+  };
+
+  type BaseResponseUserVO_ = {
+    code?: number;
+    data?: UserVO;
+    message?: string;
+  };
+
+  type Chart = {
+    chartData?: string;
+    chartState?: number;
+    chartType?: string;
+    createTime?: string;
+    execMessage?: string;
+    genChart?: string;
+    genResult?: string;
+    goal?: string;
+    id?: string;
+    isDelete?: number;
+    name?: string;
+    updateTime?: string;
+    userId?: string;
+  };
+
+  type ChartAddRequest = {
+    chartData?: string;
+    chartType?: string;
+    goal?: string;
+  };
+
+  type ChartEditRequest = {
+    chartData?: string;
+    chartType?: string;
+    goal?: string;
+    id?: string;
+  };
+
+  type ChartQueryRequest = {
+    chartType?: string;
+    current?: number;
+    goal?: string;
+    id?: string;
+    pageSize?: number;
+    sortField?: string;
+    sortOrder?: string;
+    userId?: string;
+  };
+
+  type ChartUpdateRequest = {
+    content?: string;
+    id?: string;
+    tags?: string[];
+    title?: string;
+  };
+
+  type DeleteRequest = {
+    id?: string;
+  };
+
+  type getChartByIdUsingGETParams = {
+    /** id */
+    id?: string;
+  };
+
+  type getPostVOByIdUsingGETParams = {
+    /** id */
+    id?: string;
+  };
+
+  type getUserByIdUsingGETParams = {
+    /** id */
+    id?: string;
+  };
+
+  type getUserVOByIdUsingGETParams = {
+    /** id */
+    id?: string;
+  };
+
+  type LoginUserVO = {
+    createTime?: string;
+    id?: string;
+    updateTime?: string;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type ModelAndView = {
+    empty?: boolean;
+    model?: Record<string, any>;
+    modelMap?: Record<string, any>;
+    reference?: boolean;
+    status?:
+      | 'ACCEPTED'
+      | 'ALREADY_REPORTED'
+      | 'BAD_GATEWAY'
+      | 'BAD_REQUEST'
+      | 'BANDWIDTH_LIMIT_EXCEEDED'
+      | 'CHECKPOINT'
+      | 'CONFLICT'
+      | 'CONTINUE'
+      | 'CREATED'
+      | 'DESTINATION_LOCKED'
+      | 'EXPECTATION_FAILED'
+      | 'FAILED_DEPENDENCY'
+      | 'FORBIDDEN'
+      | 'FOUND'
+      | 'GATEWAY_TIMEOUT'
+      | 'GONE'
+      | 'HTTP_VERSION_NOT_SUPPORTED'
+      | 'IM_USED'
+      | 'INSUFFICIENT_SPACE_ON_RESOURCE'
+      | 'INSUFFICIENT_STORAGE'
+      | 'INTERNAL_SERVER_ERROR'
+      | 'I_AM_A_TEAPOT'
+      | 'LENGTH_REQUIRED'
+      | 'LOCKED'
+      | 'LOOP_DETECTED'
+      | 'METHOD_FAILURE'
+      | 'METHOD_NOT_ALLOWED'
+      | 'MOVED_PERMANENTLY'
+      | 'MOVED_TEMPORARILY'
+      | 'MULTIPLE_CHOICES'
+      | 'MULTI_STATUS'
+      | 'NETWORK_AUTHENTICATION_REQUIRED'
+      | 'NON_AUTHORITATIVE_INFORMATION'
+      | 'NOT_ACCEPTABLE'
+      | 'NOT_EXTENDED'
+      | 'NOT_FOUND'
+      | 'NOT_IMPLEMENTED'
+      | 'NOT_MODIFIED'
+      | 'NO_CONTENT'
+      | 'OK'
+      | 'PARTIAL_CONTENT'
+      | 'PAYLOAD_TOO_LARGE'
+      | 'PAYMENT_REQUIRED'
+      | 'PERMANENT_REDIRECT'
+      | 'PRECONDITION_FAILED'
+      | 'PRECONDITION_REQUIRED'
+      | 'PROCESSING'
+      | 'PROXY_AUTHENTICATION_REQUIRED'
+      | 'REQUESTED_RANGE_NOT_SATISFIABLE'
+      | 'REQUEST_ENTITY_TOO_LARGE'
+      | 'REQUEST_HEADER_FIELDS_TOO_LARGE'
+      | 'REQUEST_TIMEOUT'
+      | 'REQUEST_URI_TOO_LONG'
+      | 'RESET_CONTENT'
+      | 'SEE_OTHER'
+      | 'SERVICE_UNAVAILABLE'
+      | 'SWITCHING_PROTOCOLS'
+      | 'TEMPORARY_REDIRECT'
+      | 'TOO_EARLY'
+      | 'TOO_MANY_REQUESTS'
+      | 'UNAUTHORIZED'
+      | 'UNAVAILABLE_FOR_LEGAL_REASONS'
+      | 'UNPROCESSABLE_ENTITY'
+      | 'UNSUPPORTED_MEDIA_TYPE'
+      | 'UPGRADE_REQUIRED'
+      | 'URI_TOO_LONG'
+      | 'USE_PROXY'
+      | 'VARIANT_ALSO_NEGOTIATES';
+    view?: View;
+    viewName?: string;
+  };
+
+  type OrderItem = {
+    asc?: boolean;
+    column?: string;
+  };
+
+  type PageChart_ = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: Chart[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type PagePost_ = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: Post[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type PagePostVO_ = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: PostVO[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type PageUser_ = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: User[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type PageUserVO_ = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: UserVO[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type Post = {
+    content?: string;
+    createTime?: string;
+    favourNum?: number;
+    id?: string;
+    isDelete?: number;
+    tags?: string;
+    thumbNum?: number;
+    title?: string;
+    updateTime?: string;
+    userId?: string;
+  };
+
+  type PostAddRequest = {
+    content?: string;
+    tags?: string[];
+    title?: string;
+  };
+
+  type PostEditRequest = {
+    content?: string;
+    id?: string;
+    tags?: string[];
+    title?: string;
+  };
+
+  type PostFavourAddRequest = {
+    postId?: string;
+  };
+
+  type PostFavourQueryRequest = {
+    current?: number;
+    pageSize?: number;
+    postQueryRequest?: PostQueryRequest;
+    sortField?: string;
+    sortOrder?: string;
+    userId?: string;
+  };
+
+  type PostQueryRequest = {
+    content?: string;
+    current?: number;
+    favourUserId?: string;
+    id?: string;
+    notId?: string;
+    orTags?: string[];
+    pageSize?: number;
+    searchText?: string;
+    sortField?: string;
+    sortOrder?: string;
+    tags?: string[];
+    title?: string;
+    userId?: string;
+  };
+
+  type PostThumbAddRequest = {
+    postId?: string;
+  };
+
+  type PostUpdateRequest = {
+    chartData?: string;
+    chartType?: string;
+    createTime?: string;
+    genChart?: string;
+    genResult?: string;
+    goal?: string;
+    id?: string;
+    isDelete?: number;
+    updateTime?: string;
+  };
+
+  type PostVO = {
+    content?: string;
+    createTime?: string;
+    favourNum?: number;
+    hasFavour?: boolean;
+    hasThumb?: boolean;
+    id?: string;
+    tagList?: string[];
+    thumbNum?: number;
+    title?: string;
+    updateTime?: string;
+    user?: UserVO;
+    userId?: string;
+  };
+
+  type uploadFileUsingPOSTParams = {
+    biz?: string;
+  };
+
+  type User = {
+    createTime?: string;
+    id?: string;
+    isDelete?: number;
+    updateTime?: string;
+    userAccount?: string;
+    userAvatar?: string;
+    userName?: string;
+    userPassword?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserAddRequest = {
+    userAccount?: string;
+    userAvatar?: string;
+    userName?: string;
+    userRole?: string;
+  };
+
+  type UserLoginRequest = {
+    userAccount?: string;
+    userPassword?: string;
+  };
+
+  type UserQueryRequest = {
+    current?: number;
+    id?: string;
+    mpOpenId?: string;
+    pageSize?: number;
+    sortField?: string;
+    sortOrder?: string;
+    unionId?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserRegisterRequest = {
+    checkPassword?: string;
+    userAccount?: string;
+    userPassword?: string;
+  };
+
+  type UserUpdateMyRequest = {
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+  };
+
+  type UserUpdateRequest = {
+    id?: string;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserVO = {
+    createTime?: string;
+    id?: string;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type View = {
+    contentType?: string;
+  };
+}
