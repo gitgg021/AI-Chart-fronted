a911e93cf7921c1aea85e7ff70e0fbb4de4b62fd
caoxuehan <caoxuehan@qq.com>
2024-01-17 13:02:46 +0800

    新增智能分析页面(异步提交) 路由

--- config/routes.ts
+++ config/routes.ts
@@ -9,10 +9,22 @@
     redirect: '/add_chart',
   },
   {
-    name:"智能分析",
     path: '/add_chart',
+    name:"智能分析",
     icon: "fundOutlined",
     component: './AddChart'
+  },
+  {
+    path: '/add_chart_async',
+    name:"智能分析(异步)",
+    icon: "barChart",
+    component: './AddChartAsync'
+  },
+  {
+    path: '/my_chart',
+    name:"我的图表",
+    icon: "pieChart",
+    component: './MyChart'
   },
   { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
   {
